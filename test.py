d5c69242d53d88a2e5d788128ec957df67f0610f
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2025-01-18 03:56:28 -0500

    cooking 3

--- test.py
+++ test.py
@@ -1,25 +1,43 @@
 import os
+import base64
 from groq import Groq
 
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-userInput = input("Question: ")
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role":"system",
-            "content":"you are an expert on food caloric contents."
-        },
-        {
-            "role": "user",
-            "content": userInput,
-        }
-    ],
-    model="llama3-8b-8192",
-    max_completion_tokens=1024,
-)
+def encodeImage(filepath):
+    with open(filepath, "rb") as image:
+        return base64.b64encode(image.read()).decode("utf-8")
 
-print(chat_completion.choices[0].message.content)
+
+def imageToText(client, model, base64Image, prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64Image}"},
+                    },
+                ],
+            },
+        ],
+        model=model,
+        max_completion_tokens=512,
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+visionModel = "llama-3.2-11b-vision-preview"
+llamaModel = "llama3-8b-8192"
+
+imagePath = "bigmac.jpg"
+base64Image = encodeImage(imagePath)
+# prompt = "Estimate the amount of calories present in each food item in the picture and the total amount of calories"
+prompt = "Analyze this image to estimate the total calorie content of the visible food. Clearly identify each food item, approximate portion sizes, and calculate their caloric values based on standard nutritional data. If you are uncertain about a food item, specify your assumptions. Additionally, provide a breakdown of calories per item and note any factors that might affect the accuracy, such as poor lighting, unusual plating, or ambiguous portions"
+print(imageToText(client, visionModel, base64Image, prompt))
