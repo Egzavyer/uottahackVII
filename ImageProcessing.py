15c498ad79782443d5471460491add08e1d1988a
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2025-01-18 05:39:19 -0500

    feat: adds image processing api

---
+++ ImageProcessing.py
@@ -0,0 +1,41 @@
+import os
+import base64
+from groq import Groq
+
+
+class ImageProcessing:
+    def __init__(self):
+        self.client = Groq(
+            api_key=os.environ.get("GROQ_API_KEY"),
+        )
+        self.model = "llama-3.2-11b-vision-preview"
+        self.prompt = "Analyze this image to estimate the total calorie content of the visible food. Clearly identify each food item, approximate portion sizes, and calculate their caloric values based on standard nutritional data. If you are uncertain about a food item, specify your assumptions. Additionally, provide a breakdown of calories per item and note any factors that might affect the accuracy, such as poor lighting, unusual plating, or ambiguous portions"
+
+    def encodeImage(self, filepath):
+        with open(filepath, "rb") as image:
+            return base64.b64encode(image.read()).decode("utf-8")
+
+    def imageToText(self, base64Image):
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": self.prompt},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64Image}"
+                            },
+                        },
+                    ],
+                },
+            ],
+            model=self.model,
+            max_completion_tokens=512,
+        )
+
+        return chat_completion.choices[0].message.content
+
+
+imagePath = "bigmac.jpg"
---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from ImageProcessing import ImageProcessing
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/upload/")
+async def uploadImage(file: UploadFile = File(...)):
+    img = ImageProcessing()
+    return {"message": img.imageToText(img.encodeImage(file))}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
